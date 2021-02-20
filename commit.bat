465a930fad95dc186876572f824d466dbdfbe728
Renato de Matos <dev@renato.digital>
2021-02-20 13:47:53 -0300

    2021-02-20 13:47:52

--- commit.bat
+++ commit.bat
@@ -1,5 +1,5 @@
 @echo off
 
-set bklog=%date:~6,4%-%date:~3,2%-%date:~0,2% %time:~0,2%:%time:~3,2%:%time:~12,2%"
+set name=%date:~6,4%-%date:~3,2%-%date:~0,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
 
-git add . && git commit -m "%bklog%"
+git add . && git commit -m "%name%"
